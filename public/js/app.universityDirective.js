e83597156fcfcaafe171f4d5f975c5c9c23185bb
Arjun Mitra Reddy Donthala <Arjun Mitra Reddy Donthala>
2016-08-28 18:07:27 -0400

    Fix: Directions Mobile Fixes

--- public/js/app.universityDirective.js
+++ public/js/app.universityDirective.js
@@ -22,11 +22,36 @@
     function link(scope, elem, attrs) {
         scope.getDirections = function(university) {
             var daddr = university;
-            if (navigator.geolocation) {
+            var protocol;
+            if ((navigator.platform.indexOf("iPhone") !== -1) || (navigator.platform.indexOf("iPod") !== -1)) {
+                var ver;
+                if (/iP(hone|od|ad)/.test(navigator.platform)) {
+                    var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+                    ver = [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+                }
+
+                ver = ver || [0];
+
+                if (ver[0] >= 6) {
+                    protocol = 'maps://';
+                } else {
+                    protocol = 'http://';
+
+                }
                 navigator.geolocation.getCurrentPosition(function(data){
                     if (data.coords) {
                         window.open(
-                            'https://maps.google.com/maps?saddr=' + data.coords.latitude + ',' + data.coords.longitude + '&daddr=' + daddr,
+                            'http://maps.google.com/?saddr=' + data.coords.latitude + ',' + data.coords.longitude + '&daddr=' + daddr + '&amp;ll=',
+                            '_blank'
+                        );
+                    }
+                }, function() {}, {enableHighAccuracy: true});
+            }
+            else {
+                navigator.geolocation.getCurrentPosition(function(data){
+                    if (data.coords) {
+                        window.open(
+                            'http://maps.google.com/?saddr=' + data.coords.latitude + ',' + data.coords.longitude + '&daddr=' + daddr + '&amp;ll=',
                             '_blank'
                         );
                     }
